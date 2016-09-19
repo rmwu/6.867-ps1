fcf2bdb52172cb3cc5e335f807482347dba17d3b
Menghua Wu <rmwu@mit.edu>
2016-09-19 13:53:17 -0400

    stubs for gradient descent

---
+++ P1/gradientDescent.py
@@ -0,0 +1,59 @@
+"""
+6.867 Fall 2016 - Problem Set 1
+Problem 1. Implement Gradient Descent
+"""
+
+import numpy as np
+
+##################################
+# Main Functions
+##################################
+
+def gradient_descent(objective_function, gradient_function):
+	"""
+	Parameters
+		objective_function
+			Function used to calculate how good our guess
+			is so far. In this case, our objective_function
+			is a loss function to be minimized.
+		gradient_function
+			Function used to calculate the gradient at a given
+			point.
+	"""
+	pass
+
+def update(n, Theta, Y, x, eta):
+	"""
+	update(n, Theta, Y, x, eta) returns the new x
+	value after xxxxxx
+
+	Parameters
+		n			xxxxxx
+		Theta		xxxxxx
+		Y			xxxxxx
+		x			vector to be updated
+		eta			constant step size
+	"""
+	pass
+
+##################################
+# Mathematical Implementations
+##################################
+
+##################################
+# Input Handling
+##################################
+
+def get_inputs(filename):
+	"""
+	get_inputs reads the input values from the file at
+	filename
+	"""
+	pass
+
+def main():
+	"""
+	main function takes blah blah blah
+	"""
+	pass
+
