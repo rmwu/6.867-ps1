a14cb403826de34b8fdcc801d188c57d4b9a1d27
Tony Zhang <txz@mit.edu>
2016-09-25 00:36:52 -0400

    Major refactor; sum-of-squares error
    Refactored fit functions to use generic basis functions.
    Implementation for sum of squares error for polynomial basis function
    fits, as well as gradient of this error.

--- P2/main.py
+++ P2/main.py
@@ -21,44 +21,29 @@ def powers(x, degree):
     return [x**power for power in range(degree + 1)]
     
 
-def polynomial_basis_fit(X, Y, fit_degree):
-    """
-    Performs linear basis function regression on pairs (x, y) (where both are
-    real numbers) with a polynomial basis of max `fit_degree`. See Bishop,
-    Equation 3.15
+def power_basis(degree):
+    def power_func(k):
+        return lambda x: x**k
+    return [power_func(k) for k in range(degree+1)]
 
-    :param: X: the values of x
-    :param: Y: the values of y, in the same order as their counterparts in `X`
-    :param: fit_degree: the maximum degree of the polynomial fit, so our basis
-                        functions are, as functions of x, 1, x, x^2, ..., up
-                        to x^`fit_degree`
-    :return: the maximum likelihood weight vector
-    """
-    # preconditions
+
+def basis_fit(X, Y, basis_functions):
     if len(X) != len(Y):
         raise ValueError("X and Y must have same dimensions n x 1")
 
     num_datapoints = len(X)
     y_values = Y.reshape(num_datapoints, 1)
 
-    x_with_powers = [powers(x, fit_degree) for x in X]  # design matrix
-    phi = np.array(x_with_powers)
-    pseudoinverse = np.linalg.inv(phi.T.dot(phi)).dot(phi.T)
+    design_matrix = np.array([[f(x) for f in basis_functions] for x in X])
+    pseudoinverse = np.linalg.inv(design_matrix.T.dot(design_matrix)).dot(design_matrix.T)
     max_likelihood_weights = pseudoinverse.dot(y_values)
 
-    assert len(max_likelihood_weights) == fit_degree + 1
+    assert len(max_likelihood_weights) == len(basis_functions)
     return max_likelihood_weights
 
 
-def poly_fit_plot(X, Y, fit_degree, x_arange, **kwargs):
-    """
-    Plot the maximum likelihood polynomial basis function fit of degree
-    fit_degree in red, linewidth 2
-    """
-    x_arange_powers = np.array([powers(x, fit_degree) for x in x_arange])
-    max_likelihood_weights = polynomial_basis_fit(X, Y, fit_degree)
-    y_fit_values = x_arange_powers.dot(max_likelihood_weights)
-    line, = plt.plot(x_arange, y_fit_values, **kwargs)
+def polynomial_basis_fit(X, Y, fit_degree):
+    return basis_fit(X, Y, power_basis(fit_degree))
 
 
 def cosines(x, degree):
@@ -68,45 +53,63 @@ def cosines(x, degree):
     return np.cos(np.arange(1, degree+1) * math.pi * x)
 
 
+def cosine_basis(degree):
+    def cosine_func(k):
+        return lambda x: math.cos(k*math.pi*x)
+    return [cosine_func(k) for k in range(1, degree+1)]
+
+
 def cosine_basis_fit(X, Y, fit_degree):
+    return basis_fit(X, Y, cosine_basis(fit_degree))
+
+
+def basis_fit_plot(X, Y, basis_functions, x_arange, **kwargs):
     """
-    Performs linear basis function regression on pairs (x, y) (where both are
-    real numbers) with a cosine basis. (cos(pi*x), cos(2*pi*x), ... cos(M*pi*x))
-
-    :param: X: the values of x
-    :param: Y: the values of y, in the same order as their counterparts in `X`
-    :param: fit_degree: the maximum degree of the cosine fit, so our basis
-                        functions go up to cos(fit_degree*pi*x)
-    :return: the maximum likelihood weight vector
+    Plot the maximum likelihood linear basis function regression.
     """
-    # preconditions
-    if len(X) != len(Y):
-        raise ValueError("X and Y must have same dimensions n x 1")
-
-
-    num_datapoints = len(X)
-    y_values = Y.reshape(num_datapoints, 1)
-
-    x_cosines = [cosines(x, fit_degree) for x in X]
-    phi = np.array(x_cosines)
-    pseudoinverse = np.linalg.inv(phi.T.dot(phi)).dot(phi.T)
-    max_likelihood_weights = pseudoinverse.dot(y_values)
-
-    assert len(max_likelihood_weights) == fit_degree
-    return max_likelihood_weights
-
-
-def cos_fit_plot(X, Y, fit_degree, x_arange, **kwargs):
-    """
-    Plot the maximum likelihood cosine basis function fit with
-    basis functions cos(pi x), cos(2 pi x), ... , cosin(M pi x)
-    where M = fit_degree
-    """
-    x_arange_cosines = np.array([cosines(x, fit_degree) for x in x_arange])
-    max_likelihood_weights = cosine_basis_fit(X, Y, fit_degree)
-    print("maximum likelihood weights for cosine basis\n{}\n".format(max_likelihood_weights))
-    y_fit_values = x_arange_cosines.dot(max_likelihood_weights)
+    x_arange_design_matrix = np.array([[f(x) for f in basis_functions] for x in x_arange])
+    max_likelihood_weights = basis_fit(X, Y, basis_functions)
+    print("maximum likelihood weights for basis\n{}\n".format(max_likelihood_weights))
+    y_fit_values = x_arange_design_matrix.dot(max_likelihood_weights)
     plt.plot(x_arange, y_fit_values, **kwargs)
+
+
+def design_matrix(basis_functions, X):
+    """
+    The design matrix
+    [[f0(x0), f1(x0), ...],
+     [f0(x1), f1(x1), ...],
+     ...]
+    """
+    return [[f(x) for f in basis_functions] for x in X]
+
+
+def poly_basis_square_sum_error(weight_vector, X, Y):
+    """
+    Squared sum error, given a weight vector from a polynomial basis fit
+    """
+    degree = len(weight_vector) - 1
+    return square_sum_error(design_matrix(power_basis(degree), X), Y)
+
+
+def poly_basis_square_sum_error_grad(weight_vector, X, Y):
+    """
+    Gradient of squared sum error, given a weight vector from a poly basis fit
+    """
+    assert len(Y.shape) == 2
+    assert Y.shape[1] == 1
+    degree = len(weight_vector) - 1
+    phi = design_matrix(power_basis(degree))
+    return phi.T.dot(phi.dot(weight_vector) - Y)
+
+
+def square_sum_error(y_guess, y_actual):
+    """
+    Sum of squares error, Bishop Equation 3.12.
+    :return: 1/2 * sum of squared errors.
+    """
+    error_vec = y_guess - y_actual
+    return 1/2 * error_vec.T.dot(error_vec)
 
 
 def main():
@@ -134,8 +137,8 @@ def main():
 
     # plot the fit
     fit_kwargs = {"color": 'r', "lw": 2}
-    plot_func = poly_fit_plot if poly_basis else cos_fit_plot
-    plot_func(X, Y, fit_degree, x_arange, **fit_kwargs)
+    basis_functions = power_basis(fit_degree) if poly_basis else cosine_basis(fit_degree)
+    basis_fit_plot(X, Y, basis_functions, x_arange, **fit_kwargs)
 
     # nice setup
     plt.xlabel('x')
