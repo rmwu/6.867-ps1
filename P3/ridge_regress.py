92b26ed46c7add6cf9a417e6d9578fccaf7b1f58
Menghua Wu <rmwu@mit.edu>
2016-09-25 04:42:30 -0400

    .

--- P3/ridge_regress.py
+++ P3/ridge_regress.py
@@ -3,9 +3,26 @@
 Problem 3. Implement Ridge Regression
 """
 import numpy as np
+import sys
+sys.path.append("..")
+
+import P2.poly_basis_grad_descent as pbgd
 
 def weight_regularizer(weight_vector, reg_coef):
 	"""
 	Regularization term, sum-of-squares of weight vector
 	"""
 	return 0.5 * reg_coef * weight_vector.T.dot(weight_vector)
+
+def ridge_error(weight_vector, X, Y):
+	return poly_basis_square_sum_error + weight_regularizer(weight_vector)
+
+def ridge_error_ml(phi, x, reg_coef = 0.25):
+	"""
+	max likelihood weight
+
+	x data vector
+	"""
+	n = phi.shape[0]
+	return np.linalg.inv(reg_coef * np.identity(n)+ 
+	    phi.T.dot(phi)).dot(phi.T.dot(x))
