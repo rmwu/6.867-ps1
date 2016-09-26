bb55f11f617928bff4f3d7224467a687f7b870e3
Tony Zhang <txz@mit.edu>
2016-09-26 00:06:25 -0400

    code updates

--- P1/gradientDescent.py
+++ P1/gradientDescent.py
@@ -4,171 +4,170 @@ Problem 1. Implement Gradient Descent
 """
 from __future__ import division
 import numpy as np
-from math_functions import *
 
 import pylab as pl
 import matplotlib.pyplot as plt
 
-debug = True
+debug = False
 
 ##################################
 # Input handling
 ##################################
 
-def get_raw_data():
+# def get_raw_data():
     
-    # load the fitting data for X and y and return as elements of a tuple
-    # X is a 100 by 10 matrix and y is a vector of length 100
-    # Each corresponding row for X and y represents a single data sample
+#     # load the fitting data for X and y and return as elements of a tuple
+#     # X is a 100 by 10 matrix and y is a vector of length 100
+#     # Each corresponding row for X and y represents a single data sample
 
-    X = pl.loadtxt('fittingdatap1_x.txt')
-    y = pl.loadtxt('fittingdatap1_y.txt')
+#     X = pl.loadtxt('fittingdatap1_x.txt')
+#     y = pl.loadtxt('fittingdatap1_y.txt')
 
-    return (X,y)
+#     return (X,y)
 
-def get_raw_params():
+# def get_raw_params():
 
-    # load the parameters for the negative Gaussian function and quadratic bowl function
-    # return a tuple that contains parameters for Gaussian mean, Gaussian covariance,
-    # A and b for quadratic bowl in order
+#     # load the parameters for the negative Gaussian function and quadratic bowl function
+#     # return a tuple that contains parameters for Gaussian mean, Gaussian covariance,
+#     # A and b for quadratic bowl in order
 
-    data = pl.loadtxt('parametersp1.txt')
+#     data = pl.loadtxt('parametersp1.txt')
 
-    gaussMean = data[0,:]
-    gaussCov = data[1:3,:]
+#     gaussMean = data[0,:]
+#     gaussCov = data[1:3,:]
 
-    quadBowlA = data[3:5,:]
-    quadBowlb = data[5,:]
+#     quadBowlA = data[3:5,:]
+#     quadBowlb = data[5,:]
 
-    return (gaussMean,gaussCov,quadBowlA,quadBowlb) 
+#     return (gaussMean,gaussCov,quadBowlA,quadBowlb) 
 
-def get_gaussian_params():
-    mu, sigma = get_raw_params()[:2]
-    mu = np.array(mu).reshape(2, 1)
-    sigma = np.array(sigma)
+# def get_gaussian_params():
+#     mu, sigma = get_raw_params()[:2]
+#     mu = np.array(mu).reshape(2, 1)
+#     sigma = np.array(sigma)
 
-    return (mu, sigma)
+#     return (mu, sigma)
 
-def get_quad_params():
-    a, b = get_raw_params()[2:4]
-    b = np.array(b).reshape(2, 1)
-    a = np.array(a)
+# def get_quad_params():
+#     a, b = get_raw_params()[2:4]
+#     b = np.array(b).reshape(2, 1)
+#     a = np.array(a)
 
-    return (a, b)
+#     return (a, b)
 
-def get_data():
-    x, y = get_raw_data()
-    x, y = np.array(x).reshape(100, 10), np.array(y).reshape(100, 1)
-    return (x, y)
+# def get_data():
+#     x, y = get_raw_data()
+#     x, y = np.array(x).reshape(100, 10), np.array(y).reshape(100, 1)
+#     return (x, y)
 
-mu, sigma = get_gaussian_params()
-a, b = get_quad_params()
-x_data, y_data = get_data()
+# mu, sigma = get_gaussian_params()
+# a, b = get_quad_params()
+# x_data, y_data = get_data()
 
-##################################
-# Mathematical Implementations
-##################################
+# ##################################
+# # Mathematical Implementations
+# ##################################
 
-def d_negative_gaussian(x):
-    """
-    d_negative_gaussian(x) calculates the derivative
-    of a negative gaussian function.
+# def d_negative_gaussian(x):
+#     """
+#     d_negative_gaussian(x) calculates the derivative
+#     of a negative gaussian function.
 
-    Parameters
-        x           current vector
-    """
-    n, _ = sigma.shape
-    f_x = negative_gaussian(mu)
+#     Parameters
+#         x           current vector
+#     """
+#     n, _ = sigma.shape
+#     f_x = negative_gaussian(mu)
 
-    return -f_x * np.linalg.inv(sigma).dot(x - mu)
+#     return -f_x * np.linalg.inv(sigma).dot(x - mu)
 
-def negative_gaussian(x):
-    """
-    negative_gaussian(x) calculates the negative gaussian
-    function given inputs.
+# def negative_gaussian(x):
+#     """
+#     negative_gaussian(x) calculates the negative gaussian
+#     function given inputs.
 
-    Parameters
-        x           current vector
-    """
-    n = sigma.shape[0]
+#     Parameters
+#         x           current vector
+#     """
+#     n = sigma.shape[0]
 
-    gaussian_normalization = 1/(math.sqrt((2*math.pi)**n * np.linalg.det(sigma)))
+#     gaussian_normalization = 1/(math.sqrt((2*math.pi)**n * np.linalg.det(sigma)))
     
-    exponent = -1/2 * (x - mu).T.dot(np.linalg.inv(sigma)).dot(x - mu)
+#     exponent = -1/2 * (x - mu).T.dot(np.linalg.inv(sigma)).dot(x - mu)
 
-    return -1 * gaussian_normalization * math.exp(exponent)
+#     return -1 * gaussian_normalization * math.exp(exponent)
 
-def d_quadratic_bowl(x):
-    """
-    d_quadratic_bowl(x) calculates the derivative of a 
-    quadratic bowl, which has the form:
+# def d_quadratic_bowl(x):
+#     """
+#     d_quadratic_bowl(x) calculates the derivative of a 
+#     quadratic bowl, which has the form:
 
-        del f(x) = Ax - b
+#         del f(x) = Ax - b
 
-    Parameters
-        x           n by 1
-    """
-    return a.dot(x) - b
+#     Parameters
+#         x           n by 1
+#     """
+#     return a.dot(x) - b
 
-def quadratic_bowl(x):
-    """
-    quadratic_bowl(x) calculates the quadratic bowl function,
-    which has the form:
+# def quadratic_bowl(x):
+#     """
+#     quadratic_bowl(x) calculates the quadratic bowl function,
+#     which has the form:
 
-        f(x) = 1/2 xT Ax - xT b
+#         f(x) = 1/2 xT Ax - xT b
 
-    Parameters
-        x           n by 1
-    """
-    return 0.5 * x.T.dot(a).dot(x) - x.T.dot(b)
+#     Parameters
+#         x           n by 1
+#     """
+#     return 0.5 * x.T.dot(a).dot(x) - x.T.dot(b)
 
-def d_squared_error(theta):
-    """
-    d_squared_error(theta) calculates the gradient of J(x), which is
-    the squared error:
+# def d_squared_error(theta):
+#     """
+#     d_squared_error(theta) calculates the gradient of J(x), which is
+#     the squared error:
 
-        J(x) = || x theta - y|| ^2
+#         J(x) = || x theta - y|| ^2
 
-    Parameters
-        theta       n by 1
-    """
-    return 2 * x_data.T.dot(x_data.dot(theta) - y_data)
+#     Parameters
+#         theta       n by 1
+#     """
+#     return 2 * x_data.T.dot(x_data.dot(theta) - y_data)
 
-def squared_error(theta):
-    """
-    squared_error(theta) returns the square error, defined
-    as:
+# def squared_error(theta):
+#     """
+#     squared_error(theta) returns the square error, defined
+#     as:
 
-        J(theta) = || x theta - y || ^2
+#         J(theta) = || x theta - y || ^2
 
-    Parameters
-        x       n by n
-        theta   n by 1
-        y       n by 1
-    """
-    n, m = x_data.shape
-    return np.linalg.norm(x_data.dot(theta) - y_data) ** 2
+#     Parameters
+#         x       n by n
+#         theta   n by 1
+#         y       n by 1
+#     """
+#     n, m = x_data.shape
+#     return np.linalg.norm(x_data.dot(theta) - y_data) ** 2
 
-def d_stochastic_error(theta, i):
-    xi = x_data[i].reshape(10, 1)
-    yi = y_data[i]
+# def d_stochastic_error(theta, i):
+#     xi = x_data[i].reshape(10, 1)
+#     yi = y_data[i]
 
-    return 2 * xi * (xi.T.dot(theta.reshape(10, 1))-yi)
+#     return 2 * xi * (xi.T.dot(theta.reshape(10, 1))-yi)
 
-def stochastic_error(theta, i):
-    """
-    calculates
+# def stochastic_error(theta, i):
+#     """
+#     calculates
 
-        J(x_t) = || x(i).T theta_t - y(i) || ^2
+#         J(x_t) = || x(i).T theta_t - y(i) || ^2
 
-    where t represents the iteration, and i represents the sample index
-    """
-    xiT = x_data[i].reshape(10, 1).T
-    yi = y_data[i]
+#     where t represents the iteration, and i represents the sample index
+#     """
+#     xiT = x_data[i].reshape(10, 1).T
+#     yi = y_data[i]
 
-    # print("x {} and y{}".format(xiT, yi))
-    # print("stoch error{}".format((xiT.dot(theta.reshape(10, 1))-yi) ** 2))
-    return (xiT.dot(theta.reshape(10, 1))-yi) ** 2 
+#     # print("x {} and y{}".format(xiT, yi))
+#     # print("stoch error{}".format((xiT.dot(theta.reshape(10, 1))-yi) ** 2))
+#     return (xiT.dot(theta.reshape(10, 1))-yi) ** 2 
 
 
 ##################################
@@ -187,7 +186,7 @@ def gradient_descent(x_init, objective, gradient, eta, threshold, delta, conv_by
         gradient    gradient function
         eta         step size
         threshold   convergence threshold
-        delta       numerical gradient estimate
+
         conv_by_grad true if converge by grad norm, false otherwise
         stochastic  true if stochastic updates, false by default
     """
@@ -214,9 +213,10 @@ def gradient_descent(x_init, objective, gradient, eta, threshold, delta, conv_by
 
         # estimate gradient norm, gradient
         if stochastic:
-            est_slope, est_grad = central_difference(objective, current_x, delta, i)
+            est_grad = central_difference(objective, current_x, delta, i)
         else:
-            est_slope, est_grad = central_difference(objective, current_x, delta)
+            est_grad = central_difference(objective, current_x, delta)
+        est_slope = np.linalg.norm(est_grad)
         # calculate objective function
 
         if stochastic:
@@ -226,8 +226,8 @@ def gradient_descent(x_init, objective, gradient, eta, threshold, delta, conv_by
 
         if debug:
             print("Gradient norm: {}\nCurrent X: {}\nObjective function: {}\nEstimated next gradient: {}"\
-            .format(current_norm, current_x, fx1, est_grad))
-            print("Past objective function: {}\n".format(fx0))
+                .format(current_norm, current_x, fx1, est_grad))
+            print("Past objective function: {}".format(fx0))
 
         # check for convergence
         if conv_by_grad and converge_grad(grad, threshold):
@@ -258,14 +258,16 @@ def update(gradient, x, eta, i = None, t = None):
         eta         constant step size
         i           optional, to specify stochastic
     """
+    step = eta
     if i is not None:
         assert t is not None
         grad = gradient(x, i)
-        eta = (eta + t) ** (-0.75) # adjust learning rate
+        step = (eta + t) ** (-0.75) # adjust learning rate
+        print("eta={}\n\n".format(step))
     else:
         grad = gradient(x)
 
-    x_new = x - eta * grad
+    x_new = x - step * grad
 
     return (x_new, grad)
 
@@ -273,21 +275,22 @@ def update(gradient, x, eta, i = None, t = None):
 # Estimations
 ##################################
 
-def central_difference(f, x, delta, stochI = None):
+def central_difference(f, x, delta, stochI=None):
     """
     central_difference(f, x, delta) calculates an approximation of the gradient
     at a given point, for a given function f. The central difference is defined as:
 
         (f(x + delta/2) - f(x - delta/2)) / delta
     """
-    n, m = x.shape
+    n = x.shape[0]
 
     delta_matrix = np.identity(n) * delta/2
 
     est_gradient = []
     for i in range(n):
-        new_x_pos = x + delta_matrix[i].reshape(n, 1)
-        new_x_neg = x - delta_matrix[i].reshape(n, 1)
+        ihat = delta_matrix[i].reshape(n)  # unit vector \hat{x}_i
+        new_x_pos = x + ihat
+        new_x_neg = x - ihat
 
         if stochI is not None:
             f_pos = f(new_x_pos, stochI)
@@ -299,8 +302,10 @@ def central_difference(f, x, delta, stochI = None):
         # print(f_pos - f_neg)
 
         est_gradient.append((f_pos - f_neg) / delta)
+    
 
-    return (np.linalg.norm(est_gradient), est_gradient)
+    # return (np.linalg.norm(est_gradient), est_gradient)
+    return np.array(est_gradient).reshape(n, 1)
 
 def converge_grad(grad, threshold):
     """
--- P2/poly_basis_grad_descent.py
+++ P2/poly_basis_grad_descent.py
@@ -3,8 +3,9 @@ sys.path.append("..")
 
 import numpy as np
 
-import P1.gradientDescent as gd
+from P1.gradientDescent import gradient_descent, central_difference
 import lbf_regression as lbfr
+import loadFittingDataP2 as lfd
 
 def square_sum_error(y_guess, y_actual):
     """
@@ -19,47 +20,85 @@ def poly_basis_square_sum_error(weight_vector, X, Y):
     """
     Squared sum error, given a weight vector from a polynomial basis fit
     """
+    assert (len(weight_vector.shape)==1 or weight_vector.shape[1]==1)
     degree = len(weight_vector) - 1
-    return square_sum_error(lbfr.design_matrix(power_basis(degree), X), Y)
+    Y_guess = lbfr.design_matrix(
+        lbfr.power_basis(degree), X).dot(weight_vector)
+    return square_sum_error(Y_guess, Y)
 
 
 def poly_basis_square_sum_error_grad(weight_vector, X, Y):
     """
     Gradient of squared sum error, given a weight vector from a poly basis fit
     """
-    assert len(Y.shape) == 2
-    assert Y.shape[1] == 1
     degree = len(weight_vector) - 1
-    phi = lbfr.design_matrix(power_basis(degree), X)
+    phi = lbfr.design_matrix(lbfr.power_basis(degree), X)
     return phi.T.dot(phi.dot(weight_vector) - Y)
 
 
 def verify_gradient():
-    num_datapoints = 100
-    degree = 8
+    """
+    Verifies the our gradient function.
+    """
 
-    rand_x = 20 * (np.random.rand(num_datapoints) - 0.5)
-    rand_y = 20 * (np.random.rand(num_datapoints) - 0.5)
+    num_datapoints = 5000
+    degree = 4  # deg of polynomial fit
+
+    # "observed" data points (fake data)
+    data_amplitude = 1
+    rand_x = data_amplitude * (np.random.rand(num_datapoints) - 0.5)
+    rand_y = data_amplitude * (np.random.rand(num_datapoints, 1) - 0.5)
     weight_vector = 10 * (np.random.rand(degree, 1) - 0.5)
 
     def obj_func(weights):
         return poly_basis_square_sum_error(weights, rand_x, rand_y)
-    approx_grad = gd.central_difference(obj_func, weight_vector, 1e-8)
+    approx_grad = central_difference(obj_func, weight_vector, 1e-8)
     exact_grad = poly_basis_square_sum_error_grad(weight_vector, rand_x, rand_y)
-    grad_err = approx_grad - exact_grad
-    mse = grad_err.T.dot.grad_err.mean()
 
-    print("exact gradient\n{}\napprox gradient\n{}\nmean sq err\n{}".format(approx_grad, exact_grad, mse))
-    if mse / approx_grad.T.dot.approx_grad > 1e-5:
+    grad_err = approx_grad - exact_grad
+    mse = grad_err.T.dot(grad_err).mean()
+
+    print("exact gradient\n{}\napprox gradient\n{}\nmean sq err\n{}\n\n".format(exact_grad, approx_grad, mse))
+    if mse / approx_grad.T.dot(approx_grad) > 1e-5:
         raise RuntimeError("bad gradient!")
 
 
 def main():
-    """
-    Verifies the our gradient function.
-    """
-    for i in range(int(sys.argv[1])):
-        verify_gradient()
+    # for i in range(int(sys.argv[1])):
+    #     verify_gradient()
+
+    poly_fit_degree = int(sys.argv[1])
+    X_raw, Y_raw = lfd.getData(ifPlotData=False)
+
+    assert len(X_raw) == len(Y_raw)
+    num_datapoints = len(X_raw)
+    X = np.array(X_raw).reshape(num_datapoints)
+    Y = np.array(Y_raw).reshape(num_datapoints)
+
+
+    bgd_weight_vector = gradient_descent(
+        np.zeros(poly_fit_degree + 1),  # initial guess
+        lambda w: poly_basis_square_sum_error(w, X, Y),  # obj function
+        lambda w: poly_basis_square_sum_error_grad(w, X, Y),
+        0.05, # step size
+        1e-5, # convergence threshold
+        0.05, # gradient approximation parameter (for central diff)
+        False, # no convergence by grad norm
+        stochastic=False
+        )
+
+    sgd_weight_vector = gradient_descent(
+        np.zeros(poly_fit_degree + 1),  # initial guess
+        lambda w, i: poly_basis_square_sum_error(w, np.array([X[i]]), np.array([Y[i]])),  # obj function
+        lambda w, i: poly_basis_square_sum_error_grad(w, np.array([X[i]]), np.array([Y[i]])),
+        0.01, # step size
+        1e-5, # convergence threshold
+        0.05, # gradient approximation parameter (for central diff)
+        False, # no convergence by grad norm
+        stochastic=True
+        )
+
+
 
 
 if __name__ == '__main__':
